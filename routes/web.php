f979c460c58c1ab34bac6e7f0d19247f2df71fba
periyasamy <periyasamy20008732@gmail.com>
2026-09-18 16:43:04 +0530

    Add fallback route to redirect to home

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PortfolioController::class, 'index'])->name('portfolio');
+Route::fallback(function () {
+    return redirect('/');
+});
