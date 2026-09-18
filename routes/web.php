ff86069e13ffa040ba7102199b308fcd40e19c0c
periyasamy <periyasamy20008732@gmail.com>
2026-09-18 16:47:47 +0530

    Update fallback route to return portfolio index

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,8 @@
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Add this route to your project's routes/web.php
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/', [PortfolioController::class, 'index'])->name('portfolio');
+
 Route::fallback(function () {
-    return redirect('/');
+    return app(PortfolioController::class)->index();
 });
